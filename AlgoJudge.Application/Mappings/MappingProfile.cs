f2ef8874bc2a96b97d1a45571d871e5bab936bfc
phom77 <bdkhanh2005@gmail.com>
2026-03-20 20:07:32 +0700

    feat: intergrate AutoMapper for problem Mapping

---
+++ AlgoJudge.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AlgoJudge.Application.DTOs.Problem;
+using AlgoJudge.Domain.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoJudge.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Problem, ProblemDto>();
+
+            CreateMap<CreateProblemDto, Problem>();
+        }
+    }
+}
